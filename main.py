a50607cd6ce45b675bb63a12596cef4e4d977d3f
Michael Ganzorig <manlai1025@gmail.com>
2018-09-13 13:32:34 -0700

    simplifying the ui

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 import pygame, sys, random, math
-width = 500
-height = 500
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+WIDTH_PLAYER = 50
+PLAYER_SPEED = 10
 
 
 def drawAll(screen):
-    screen.fill((56, 142, 60))
-    pygame.draw.circle(screen, (255, 255, 255), [int(width/2), int(height/2)], 50, 3)  # halfcourt circle
-    pygame.draw.line(screen, (255, 255, 255), (int(width/2), 0), (int(width/2), height), 3)  # halfcourt line
-    # pygame.draw.rect(screen, (255, 255, 255), [0, 0, width, height], 10)  # border
-    # pygame.draw.rect(screen, (56, 142, 60), [0, 200, 10, 100])
-    # pygame.draw.rect(screen, (56, 142, 60), [0, 0, 10, height])
-    # pygame.draw.rect(screen, (56, 142, 60), [widht - 10, 200, 10, 100])
-    # pygame.draw.rect(screen, (56, 142, 60), [width - 10, 0, 10, height])
+    screen.fill((0, 0, 0))
 
 
 def main():
     pygame.init()
     pygame.display.set_caption("Ping Pong")
-    screen = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fpsController = pygame.time.Clock()
     positionP1 = 230
     positionP2 = 230
     ballSpeed = 4
-    positionBall = [int(width/2), int(height/2)]
+    positionBall = [int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2)]
     ballAngle = random.random() + 1
     print("Angle is: ", ballAngle)
     while True:
         drawAll(screen)
-        ball = pygame.draw.circle(screen, (255, 0, 0), positionBall, 10)
-        player1 = pygame.draw.rect(screen, (0, 0, 255), [15, positionP1, 10, 40])
-        player2 = pygame.draw.rect(screen, (0, 0, 255), [width - 22, positionP2, 10, 40])
+        ball = pygame.draw.circle(screen, (255, 255, 255), positionBall, 10)
+        player1 = pygame.draw.rect(screen, (255, 255, 255), [15, positionP1, 10, WIDTH_PLAYER])
+        player2 = pygame.draw.rect(screen, (255, 255, 255), [SCREEN_WIDTH - 22, positionP2, 10, WIDTH_PLAYER])
 
-        positionBall[0] += int(ballSpeed * math.cos(ballAngle))
-        positionBall[1] += int(ballSpeed * math.sin(ballAngle))
+        #positionBall[0] += int(ballSpeed * math.cos(ballAngle))
+        #positionBall[1] += int(ballSpeed * math.sin(ballAngle))
 
         if positionBall[0] + 10 >= 500:
             ballSpeed = -4
@@ -53,16 +48,16 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             if positionP1 >= 0:
-                positionP1 -= 5
+                positionP1 -= PLAYER_SPEED
         if keys[pygame.K_s]:
-            if positionP1 + 40 <= height:
-                positionP1 += 5
+            if positionP1 + 40 <= SCREEN_HEIGHT:
+                positionP1 += PLAYER_SPEED
         if keys[pygame.K_UP]:
             if positionP2 >= 0:
-                positionP2 -= 5
+                positionP2 -= PLAYER_SPEED
         if keys[pygame.K_DOWN]:
-            if positionP2 + 40 <= height:
-                positionP2 += 5
+            if positionP2 + 40 <= SCREEN_HEIGHT:
+                positionP2 += PLAYER_SPEED
         if keys[pygame.K_SPACE]:
             ballAngle = random.random() + 1
 
