887695c69562c99f47134aad443c3e70bff57fb9
Michael Ganzorig <manlai1025@gmail.com>
2018-09-19 23:53:06 -0700

    more datasets

--- main.py
+++ main.py
@@ -10,27 +10,27 @@ BALL_SPEED = 10
 WHITE = (255, 255, 255)
 
 def getLabel():
-	# return 'u' for UP
-	# return 'd' for DOWN
-	# return 's' for STILL
-	global positionBall, ballSpeed
-	global positionP2
-	if ballSpeed[0] > 0 and ballSpeed[1] > 0 and positionBall[1] > positionP2[1]:
-		return 'd'
+    # return 'u' for UP
+    # return 'd' for DOWN
+    # return 's' for STILL
+    global positionBall, ballSpeed
+    global positionP2
+    if ballSpeed[0] > 0 and ballSpeed[1] > 0 and positionBall[1] > positionP2[1]:
+        return 'd'
 
 def resetGame():
-	global positionBall, ballSpeed
-	positionBall = [int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2)]
-	ballSpeed = [BALL_SPEED, BALL_SPEED]
+    global positionBall, ballSpeed
+    positionBall = [int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2)]
+    ballSpeed = [BALL_SPEED, BALL_SPEED]
 
 
 pygame.init()
 pygame.display.set_caption("Ping Pong")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-countUp = 0
-countDown = 0
-countStill = 0
+countUp = 122
+countDown = 115
+#countStill = 9
 
 positionP1 = [35, 230]
 positionP2 = [SCREEN_WIDTH - 35, 230]
@@ -71,7 +71,7 @@ while True:
 
 
     # checking user input to move the players
-	# if UP or DOWN is pressed, a new image for a dataset is created, it will be used to train the neural network
+    # if UP or DOWN is pressed, a new image for a dataset is created, it will be used to train the neural network
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         if positionP1[1] >= 0:
@@ -92,16 +92,18 @@ while True:
         pygame.image.save(screen, "Dataset/" + "d-" + str(countDown) + ".jpg")
         if positionP2[1] + HEIGHT_PLAYER <= SCREEN_HEIGHT:
             positionP2[1] += PLAYER_SPEED
+    #else:
+        #countStill += 1
+        #pygame.image.save(screen, "Dataset/" + "s-" + str(countStill) + ".jpg")
 
     if keys[pygame.K_SPACE]:
         positionBall = [int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2)]
-
-    countStill += 1
-    if countStill < 10:
-    	pygame.image.save(screen, "Dataset/" + "s-" + str(countStill) + ".jpeg")
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("countUp is at: " + str(countUp))
+            print("countDown is at: " + str(countDown))
+            #print("countStill is at: " + countStill)
             pygame.quit()
             sys.exit()
 
